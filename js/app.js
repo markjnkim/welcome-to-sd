756fb96b250681146658727681436467635c818d
trumpocalyspe <mark.jn.kim@gmail.com>
2018-07-15 22:36:01 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCLj1tOhXfmkuTeYTGpWiJUCBCMggln6qU",
+    authDomain: "airbnb-sd.firebaseapp.com",
+    databaseURL: "https://airbnb-sd.firebaseio.com",
+    projectId: "airbnb-sd",
+    storageBucket: "airbnb-sd.appspot.com",
+    messagingSenderId: "672376233413"
+  };
+  firebase.initializeApp(config);
