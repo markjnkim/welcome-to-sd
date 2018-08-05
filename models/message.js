f6931c08139733684429cbf978876727855ed6d7
trumpocalyspe <mark.jn.kim@gmail.com>
2018-08-04 23:22:28 -0700

    linking front end to back end

--- models/message.js
+++ models/message.js
@@ -1,37 +1,37 @@
-module.exports = function (sequelize, DataTypes) {
-    var Message = sequelize.define("Message", {
-        body: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        subject: {
-            type: DataTypes.STRING(255),
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        email: {
-            type: DataTypes.STRING(255),
-                allowNull: true,
-                validate: {
-                    len: [1],
-                    isEmail: true
-                }
-        }
-    }, {
-            timestamps: false
-        });
+module.exports = (sequelize, DataTypes) => {
+  var Message = sequelize.define("Message", {
+    body: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    subject: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      validate: {
+        len: [1],
+        isEmail: true
+      }
+    }
+  }, {
+      timestamps: false
+    });
 
-    // Messages have one author
-    Message.associate = models => {
-        Message.belongsTo(models.Author, {
-            onDelete: "cascade"
-        });
-    };
+  // Messages have one author
+  Message.associate = models => {
+    Message.belongsTo(models.Author, {
+      onDelete: "cascade"
+    });
+  };
 
-    return Message;
+  return Message;
 };
