b78bc4dd11e9bd0752f83587057753fb4625b8b5
trumpocalyspe <mark.jn.kim@gmail.com>
2018-08-06 15:58:04 -0700

    connecting backend using jQuery

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -3,7 +3,7 @@ table = require("console.log");
 $(document).ready( function() {
 // Get all message from guest
   $.get("/api/messages/all", function(data) {
-    res.send(data);
+    
     console.table(data);
   })
 
